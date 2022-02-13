92391690c31ec83a6962484357d134d55807c0f0
Matthes <matthes@leuffen.de>
2022-02-13 19:00:05 +0100

    update

--- src/formmail.js
+++ src/formmail.js
@@ -69,55 +69,58 @@ class MicxFormmail extends HTMLElement {
   }
 
   connectedCallback() {
-    let fe = this.formEl = this.parentElement;
-    this._log("Micx formmailer ", this, "initializing on form ", fe);
-    if (fe.tagName !== "FORM")
-      throw ["Invalid parent node tagName (!= 'form') on MicxFormmailer node: ", this, "Parent is", fe];
+    document.addEventListener("DOMContentLoaded" () => {
+      let fe = this.formEl = this.parentElement;
+      this._log("Micx formmailer ", this, "initializing on form ", fe);
+      if (fe.tagName !== "FORM")
+        throw ["Invalid parent node tagName (!= 'form') on MicxFormmailer node: ", this, "Parent is", fe];
 
-    fe.addEventListener("submit", (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-    });
+      fe.addEventListener("submit", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      });
 
-    for (let sbe of fe.querySelectorAll("input[type='submit'], button[type='submit']")) {
-      this._log("Micx formmailer ", this, "attached to button", sbe);
-      sbe.addEventListener("click", (e) => {
+      for (let sbe of fe.querySelectorAll("input[type='submit'], button[type='submit']")) {
+        this._log("Micx formmailer ", this, "attached to button", sbe);
+        sbe.addEventListener("click", (e) => {
 
-        let formData = this._getFormData();
-        this._log("Micx formmailer ", this, "onclick event:", e, "formdata:", formData);
+          let formData = this._getFormData();
+          this._log("Micx formmailer ", this, "onclick event:", e, "formdata:", formData);
 
-        if (this.invalidForms.length > 0) {
-          console.warn("Form data is invalid", this.invalidForms);
-          this.dispatchEvent(new Event("invalid", {invalid_forms: this.invalidForms}));
-          return;
-        }
-
-        fetch(this.attrs.endpoint_url, {
-          method: "POST",
-          headers: {"content-type": "application/json"},
-          body: JSON.stringify(formData),
-          cache: "no-cache"
-        }).then((ret) => {
-          if (ret.status !== 200) {
-            this.dispatchEvent(new Event("error", {server_response: ret}));
-            console.log("Unable to send mail: Response", ret);
+          if (this.invalidForms.length > 0) {
+            console.warn("Form data is invalid", this.invalidForms);
+            this.dispatchEvent(new Event("invalid", {invalid_forms: this.invalidForms}));
             return;
           }
-          this._log("Micx formmailer ", this, "received ok from server", ret, "fireing success event");
-          this.dispatchEvent(new Event("success", {server_response: ret}));
-          fe.querySelector(this.attrs.ok_elem_selector)?.removeAttribute("hidden");
-        }).catch((e) => {
-          this.dispatchEvent(new Event("error", {exception: e}));
-          console.error("Micx Formmailer: request error:", e, "on element", fe);
-          alert("Unable to send mail: Server Error.");
-        })
-      })
-    }
 
-    // Hide everything inside
-    for (let sube of this.querySelectorAll("*")) {
-      sube.setAttribute("hidden", "hidden");
-    }
+          fetch(this.attrs.endpoint_url, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(formData),
+            cache: "no-cache"
+          }).then((ret) => {
+            if (ret.status !== 200) {
+              this.dispatchEvent(new Event("error", {server_response: ret}));
+              console.log("Unable to send mail: Response", ret);
+              return;
+            }
+            this._log("Micx formmailer ", this, "received ok from server", ret, "fireing success event");
+            this.dispatchEvent(new Event("success", {server_response: ret}));
+            fe.querySelector(this.attrs.ok_elem_selector)?.removeAttribute("hidden");
+          }).catch((e) => {
+            this.dispatchEvent(new Event("error", {exception: e}));
+            console.error("Micx Formmailer: request error:", e, "on element", fe);
+            alert("Unable to send mail: Server Error.");
+          })
+        })
+      }
+
+      // Hide everything inside
+      for (let sube of this.querySelectorAll("*")) {
+        sube.setAttribute("hidden", "hidden");
+      }
+      
+    })
   }
 
 
