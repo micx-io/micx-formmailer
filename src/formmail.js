00c5eaebb4847972d597b5202a21376598f961f0
Matthias <m@tth.es>
2022-05-18 21:42:40 +0200

    update

--- src/formmail.js
+++ src/formmail.js
@@ -51,10 +51,12 @@ class MicxFormmail extends HTMLElement {
         this.innerHTML = "";
 
         fe.addEventListener("click", async (e) => {
+            console.log ("micx", e);
             if (typeof e.explicitOriginalTarget === "undefined")
                 return false;
             if ( ! MicxFormmail.isFormButtonDescendant(e.explicitOriginalTarget))
                 return false;
+            console.log ("micx in", e);
             e.preventDefault();
             e.target.setAttribute("disabled", "disabled");
 
